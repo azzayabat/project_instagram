dcebe2f3d44510d3459ea4bd9eb0fca092f44dc7
Azzaya Bat-erdene <azzayaa.bat@gmail.com>
2021-09-08 17:53:03 +0800

    login page

--- instagram/src/App.tsx
+++ instagram/src/App.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import "./App.css";
+import { FaFacebookSquare } from "react-icons/fa";
 
 function App() {
   return (
     <div className="container">
       <div className="borderIn">
-        <p>Instagram</p>
-        <div>
-          <input type="text" name="e-mail" placeholder="email"></input>
+        <div className="header">
+          <h1 id="title">Instagram</h1>
         </div>
-        <div>
-          <input type="text" name="e-mail" placeholder="password"></input>
+        <div className="inputArea">
+          <input
+            type="text"
+            name="e-mail"
+            placeholder="Phone number, username, or email"
+          ></input>
+          <input type="text" name="e-mail" placeholder="Password"></input>
+          <button id="login">Log In</button>
+          <div className="divider">
+            <div>OR</div>
+          </div>
+          <button id="loginFB">
+            <span>
+              <FaFacebookSquare size="20px" id="fbIcon" />
+            </span>
+            <span>Log in with Facebook</span>
+          </button>
+          <a className="forgetPass" href="/">
+            Forget password?
+          </a>
         </div>
-        <button>Log In</button>
       </div>
     </div>
   );
