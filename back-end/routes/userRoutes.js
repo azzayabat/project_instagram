63da49b3cef4ab96c41aec57cbe87fc48d8b3ffb
Azzaya Bat-erdene <azzayaa.bat@gmail.com>
2021-09-10 18:56:00 +0800

    login api

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -2,6 +2,7 @@ const { request } = require("express");
 const express = require("express");
 const userModel = require("../models/users");
 const bcrypt = require("bcryptjs");
+const e = require("express");
 
 const app = express();
 var salt = bcrypt.genSaltSync(10);
@@ -27,8 +28,31 @@ app.post("/user", async (req, res) => {
     res.send(user);
     console.log("saved");
   } catch (error) {
-    res.status(500).send(error);
+    // console.log("error", error.code);
+    if (error.code === 11000) {
+      res.status(500).send({ error: "email is already in use" });
+    } else {
+      res.status(500).send(error);
+    }
   }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  //finds the user and returns it as user
+  const user = userModel.findOne({ email }, (err, doc) => {
+    if (err) {
+      console.log("error");
+    } else {
+      // console.log("doc", doc);
+      if (!doc) {
+        console.log("invalid user");
+      } else {
+        return doc;
+      }
+    }
+  });
 });
 
 module.exports = app;
