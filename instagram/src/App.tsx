a513e211c9fe0edb1deb92660aaeb7efd41c7a7f
Azzaya Bat-erdene <azzayaa.bat@gmail.com>
2021-09-10 18:56:54 +0800

    login page

--- instagram/src/App.tsx
+++ instagram/src/App.tsx
@@ -1,89 +1,16 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
-import { FaFacebookSquare } from "react-icons/fa";
-import axios from "axios";
+import React from "react";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const App = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [result, setResult] = useState({});
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/user")
-  //     .then((response) => response.json())
-  //     .then((data) => setResult(data.total));
-  // }, []);
-  console.log(email);
-  const LogIn = () => {
-    // e.preventDefault();
-    console.log("login");
-    const data = {
-      username: username,
-      email: email,
-    };
-    console.log("data", data);
-    if (data) {
-      axios
-        .post("http://localhost:5000/user", data, {
-          headers: { "Content-Type": "application/json" },
-        })
-        .then((response) => {
-          // response.send(data);
-          console.log("res:", response);
-        })
-        .catch((error) => {
-          console.log("error hahhahahhah=====:", error);
-        });
-    }
-  };
   return (
-    <div className="container">
-      <div className="borderIn">
-        <div className="header">
-          <h1 id="title">Instagram</h1>
-        </div>
-        {/* <div className="inputArea"> */}
-        <form className="inputArea">
-          <input
-            type="text"
-            name="e-mail"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="username"
-          ></input>
-          <input
-            type="text"
-            name="password"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="email"
-          ></input>
-
-          <button type="button" id="login" onClick={LogIn}>
-            Log In
-          </button>
-          <div className="divider">
-            <div>OR</div>
-          </div>
-          <button id="loginFB">
-            <span>
-              <FaFacebookSquare size="20px" id="fbIcon" />
-            </span>
-            <span>Log in with Facebook</span>
-          </button>
-          <a className="forgetPass" href="/">
-            Forget password?
-          </a>
-        </form>
-        {/* </div> */}
-      </div>
-      <div className="bottomBox">
-        <p id="bottomTitle">
-          Don't have an account?{" "}
-          <a onClick={() => console.log("clicked")}>Sign Up</a>
-        </p>
-      </div>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+    </Router>
   );
 };
 
---
+++ instagram/src/pages/Login.tsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import "./SignUp.css";
+import { FaFacebookSquare } from "react-icons/fa";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [result, setResult] = useState({});
+
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/user")
+  //     .then((response) => response.json())
+  //     .then((data) => setResult(data.total));
+  // }, []);
+
+  const LogIn = () => {
+    // e.preventDefault();
+    console.log("login");
+    const data = {
+      email: email,
+      password: password,
+    };
+    console.log("data", data);
+    if (data) {
+      axios
+        .post("http://localhost:5000/login", data, {
+          headers: { "Content-Type": "application/json" },
+        })
+        .then((response) => {
+          // response.send(data);
+          console.log("res:", response);
+        })
+        .catch((error) => {
+          console.log("======error=====:", error);
+        });
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="borderIn">
+        <div className="header">
+          <h1 id="title">Instagram</h1>
+        </div>
+        {/* <div className="inputArea"> */}
+        <form className="inputArea">
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+          ></input>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          ></input>
+
+          <button type="button" id="login" onClick={LogIn}>
+            Log in
+          </button>
+          <div className="divider">
+            <div>OR</div>
+          </div>
+          <button id="loginFB">
+            <span>
+              <FaFacebookSquare size="20px" id="fbIcon" />
+            </span>
+            <span>Log in with Facebook</span>
+          </button>
+          <a className="forgetPass" href="/">
+            Forget password?
+          </a>
+        </form>
+        {/* </div> */}
+      </div>
+      <div className="bottomBox">
+        <p id="bottomTitle">
+          Don't have an account?{" "}
+          <Link to="/signup">
+            <a onClick={() => console.log("clicked")}>Sign Up</a>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
